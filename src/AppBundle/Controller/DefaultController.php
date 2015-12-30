7730ebf2d7c444b0184da3efa3c86dccfcc5de70
Karol <karol.filip.wojcik@gmail.com>
2015-12-30 10:59:02 +0100

    Set up application with basic views and routes, added FOSUserBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,7 +11,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        return $this->render('AppBundle:Default:main.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
